251cffb9486a85a71f0015b567ca328fa90e9d97
s3rpenti <hocunatrassu@gmail.com>
2026-03-03 21:10:05 +0300

    Add files via upload

---
+++ PR3.js
@@ -0,0 +1,41 @@
+// Шаг 1. Создание объекта
+const student = {
+  id: Date.now(),
+  name: "anna",
+  prefix: "ann",
+  status: "active",
+  getStatus() {
+    return this.status;
+  }
+};
+
+console.log(student);
+
+// Сюда нужно вставить вывод после первого запуска
+const SNAPSHOT_STEP1 = `{
+  id: 1772560946093,
+  name: 'anna',
+  prefix: 'ann',
+  status: 'active',
+  getStatus: [Function: getStatus]
+}`;
+
+
+// Шаг 2. Изменение состояния
+student.deactivate = function () {
+  this.status = "inactive";
+};
+
+student.deactivate();
+
+console.log(student);
+
+// Сюда нужно вставить вывод после второго запуска
+const SNAPSHOT_STEP2 = `{
+  id: 1772560946093,
+  name: 'anna',     
+  prefix: 'ann',    
+  status: 'inactive',
+  getStatus: [Function: getStatus],
+  deactivate: [Function (anonymous)]
+}`;
